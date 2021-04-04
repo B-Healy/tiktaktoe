1b0031e16ed850b09f5da2871f97d870c718e548
B-Healy <sba21164@student.cct.ie>
2021-04-04 18:06:03 +0000

    seting up board and defining print board function

---
+++ CA2tiktaktoe.py
@@ -0,0 +1,16 @@
+# set up board 
+board = {1: ' ', 2: ' ', 3: ' ',
+         4: ' ', 5: ' ', 6: ' ',
+         7: ' ', 8: ' ', 9: ' ',}
+
+
+#print out the board 
+def print_board(board):
+    print(board[1]+ '|' + board[2] + '|' + board[3])
+    print('-+-+-')
+    print(board[4]+ '|' + board[5] + '|' + board[6])
+    print('-+-+-')
+    print(board[7]+ '|' + board[8] + '|' + board[9])
+    print("\n")
+    
+print_board(board)
